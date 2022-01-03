3056cf5ae06ffa7862d51ea1b582643356b6ce37
Alexander Drager <szdrager@gmail.com>
2022-01-03 21:11:06 +0700

    Update tests with 'assert.strictEqual' function

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,9 +1,9 @@
 import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
-assert.equal(capitalize('hello'), 'Hello', 'The function is not working correctly!');
+assert.strictEqual(capitalize('hello'), 'Hello', 'The function is not working correctly!');
 
-assert.equal(capitalize(''), '', 'The function is not working correctly!');
+assert.strictEqual(capitalize(''), '', 'The function is not working correctly!');
 
 
 console.log('All tests are passed!');
